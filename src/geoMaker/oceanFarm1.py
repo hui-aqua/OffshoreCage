858b0800c880e579661d47842f1e9661f06fbb28
Izwan Ahmad <78489207+Izwan1005@users.noreply.github.com>
2022-09-26 14:42:56 +0200

    do some jobs

--- src/geoMaker/oceanFarm1.py
+++ src/geoMaker/oceanFarm1.py
@@ -1,29 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
+import pyvista as pyv
 import src.visualization.saveVtk as sv
-# default global values, can be changed.
-net_diameter = 110.0  # [m]
-net_height = 35  # [m]
-ver_section = 5  # 64
-cir_section = 8  # 17
-
+import csv
 
 # private function
 def __gen_points():
-    point_one_cage = []
-    for j in range(ver_section+1):
-        for i in range(0, cir_section):
-            point_one_cage.append(
-                [net_diameter/2.0 * np.cos(i * 2 * np.pi / float(cir_section)),
-                 net_diameter/2.0 * np.sin(i * 2 * np.pi / float(cir_section)),
-                 - j * net_height / float(ver_section)])
+    with open('point.csv', 'r') as pf:
+        next(pf)
+        point_data = csv.reader(pf)
+        point_one_cage = []
+        for line in point_data:
+            point_one_cage.append([float(x) for x in line])
     return point_one_cage
-## todo: make connnection and also net panels for cage and moorinng lines
 
-    
 
-if __name__ == "__main__":
-    point=__gen_points()
-    print(point)
-    sv.write_vtkPoint(point)
+def __gen_lines():    
+    with open('lines.csv', 'r') as lf:
+        next(lf)
+        lines_data = csv.reader(lf)
+        lines = []
+        for line in lines_data:
+            lines.append([int(x) for x in line])
+    return lines
+
+
+def __gen_surfs():
+    with open('surfs.csv', 'r') as sf:
+        next(sf)
+        surfs_data = csv.reader(sf)
+        surf_element = []
+        for line in surfs_data:
+            surf_element.append([int(x) for x in line])
+    return surf_element
+
+# public function
+def gen_lines():
+    lines=__gen_lines()
+    return lines
+
+def gen_cage():
+    points = __gen_points()
+    surfs = __gen_surfs()
+    return points, surfs
